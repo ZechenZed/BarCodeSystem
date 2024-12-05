4c076b1db555e4939285d217934795337a1c7cd6
zzhang01 <fluffyzed2@gmail.com>
2024-12-05 15:09:26 -0500

    update on 2024/12/5

--- data_collection.py
+++ data_collection.py
@@ -15,19 +15,15 @@ month = time.localtime().tm_mon
 day = time.localtime().tm_mday
 
 
-def time_correct(end_time_local, start_time_local):
-    cost_hour = end_time_local.tm_hour - start_time_local.tm_hour
-    cost_min = end_time_local.tm_min - start_time_local.tm_min
-    cost_sec = end_time_local.tm_sec - start_time_local.tm_sec
-    if cost_sec < 0:
-        cost_sec = 60 - abs(cost_sec)
-        cost_min -= 1
+def time_correct(start_hour, start_min, end_hour, end_min):
+    cost_hour = end_hour - start_hour
+    cost_min = end_min - start_min
     if cost_min < 0:
         cost_min = 60 - abs(cost_min)
         cost_hour -= 1
     if cost_hour < 0:
         cost_hour = 24 - abs(cost_hour)
-    return cost_hour, cost_min, cost_sec
+    return cost_hour * 60 + cost_min
 
 
 def continuous_scanning():
@@ -70,21 +66,20 @@ def missing_data_processing(data):
 
 def list_to_dic(input_list):
     log_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-
     for i in range(0, len(input_list), 3):
         temp_sale = input_list[i]
         temp_employee = input_list[i + 1]
         temp_work = input_list[i + 2]
-        log_dict[temp_sale[0]][temp_employee[0]][temp_work[0]].extend(temp_work[1])
+        if len(temp_work[0]) == 15:
+            temp_work_pack = list(temp_work[1])
+            temp_stat = temp_work[0][3:6]
+            temp_work_pack.insert(0, temp_stat)
+            log_dict[temp_sale[0]][temp_employee[0]][temp_work[0][7:]].append(temp_work_pack)
+        else:
+            log_dict[temp_sale[0]][temp_employee[0]][temp_work[0]].append(temp_work[1])
+
     with open(f"{year}_{month}_{day}_WorkLog.json", 'w') as json_file:
         json.dump(log_dict, json_file, indent=6, separators=(", ", " : "))
-        # if temp_sale[0] in log_dict:
-        #     if temp_employee[0] in log_dict[temp_sale[0]]:
-        #         log_dict[temp_sale[0]][temp_employee[0]][temp_work[0]].extend([temp_work[1]])
-        #     else:
-        #         log_dict[temp_sale[0]][temp_employee[0]][temp_work[0]] = [temp_work[1]]
-        # else:
-        #     log_dict[temp_sale[0]][temp_employee[0]][temp_work[0]] = [temp_work[1]]
     return log_dict
 
 
@@ -94,46 +89,132 @@ def main():
     input_data_fixed_dict = list_to_dic(input_data_fixed)
     pprint.pprint(input_data_fixed_dict)
 
+    # Create the structure for later transformation to pandas data structure.
     line_count = len(input_data_fixed) // 3
-    template = np.zeros((line_count, 18))
-    for i in range(0, line_count, 3):
-        temp_cell = input_data_fixed[i]
-        temp_employee = input_data_fixed[i+1]
-        temp_workID = input_data_fixed[i+2]
+    template = np.zeros((line_count, 18)).astype(str)
+    print("Start Data Processing")
 
-        # if any of the data is missing
-        if temp_cell[0] == "-" or temp_workID[0] == "-" or temp_employee[0] == "-":
-            row = np.where(template[:, 12] == 0)[0][0]
-            template[row,0] = temp_cell[0]
-            template[row,2] = temp_workID[0]
-            template[row,3] = temp_employee[0]
-            for temp_time in [temp_cell[1],temp_employee[1],temp_workID[1]]:
-                if temp_time != "-":
-                    break
-            if "END" in temp_workID[0]:
-                template[row,13] = f"{temp_time[0]}/{temp_time[1]}/{temp_time[2]}"
-                template[row,14] = f"{temp_time[3]}:{temp_time[4]}"
-            else:
-                template[row,11] = f"{temp_time[0]}/{temp_time[1]}/{temp_time[2]}"
-                template[row,12] = f"{temp_time[3]}:{temp_time[4]}"
-            continue
+    for SO_key in input_data_fixed_dict:
+        print(f"Processing {SO_key}")
+        for employee_key in input_data_fixed_dict[SO_key]:
+            print(f"    Processing {employee_key}")
+            for workid_key in input_data_fixed_dict[SO_key][employee_key]:
+                print(f"        Processing {workid_key}")
+                temp_time_list = input_data_fixed_dict[SO_key][employee_key][workid_key]
 
-        temp = template[np.where(template == input_data_fixed[i])]
-        if not len(temp):
-            temp = template[np.where(temp == input_data_fixed[i + 1])]
-        else:
-            first_empty_line = list(np.where(template[:, 0] == 0))[0]
-            template[first_empty_line][0] = input_data_fixed[i]
-            template[first_empty_line][2] = input_data_fixed[i + 2]
-            template[first_empty_line][3] = input_data_fixed[i + 1]
+                start_end_error = False if len(temp_time_list) % 2 == 0 else True
+                for i in range(len(temp_time_list)):
+                    if i % 2 == 0:
+                        if temp_time_list[i][0] != "STA":
+                            start_end_error = True
+                            break
+                    else:
+                        if temp_time_list[i][0] != "END":
+                            start_end_error = True
+                            break
 
-    data_frame = np.array([[1, 2], [1, 4], [5, 6]])
-    # filter = data_frame[np.where(data_frame[np.where(data_frame == 0)[0]] == 4)[0]]
-    test = list(np.where(data_frame == 0))[0]
-    if len(test):
-        print(1)
-    # print(filter)
+                # if any of the data is missing
+                if SO_key == "-" or employee_key == "-" or workid_key == "-" or start_end_error:
+                    row = np.where(template[:, 12] == "0.0")[0][0]
+                    template[row, 0] = SO_key
+                    template[row, 2] = workid_key
+                    template[row, 3] = employee_key
+
+                    for j in range(len(temp_time_list)):
+                        temp_time = temp_time_list[j]
+                        if temp_time != "-":
+                            if "END" in temp_time[0]:
+                                template[row, 10] = 1
+                                template[row, 13] = f"{temp_time[1]}/{temp_time[2]}/{temp_time[3]}"
+                                template[row, 14] = f"{temp_time[4]}:{temp_time[5]}"
+                            else:
+                                template[row, 10] = 1
+                                template[row, 11] = f"{temp_time[1]}/{temp_time[2]}/{temp_time[3]}"
+                                template[row, 12] = f"{temp_time[4]}:{temp_time[5]}"
+                        template[row, 17] = "Miss Data"
+                else:
+                    rows = np.where(template == SO_key)[0]
+                    row = np.where(template[rows] == workid_key)[0]
+                    print(len(template[row]))
+                    # Has the record before.
+                    if len(template[row]) != 0:
+                        # decide the location for the new employee
+                        temp_new_employee_ind = np.where(template[row[0]][3:11] == "0.0")[0][0]
+                        template[row, temp_new_employee_ind] = employee_key
+
+                        cost_min_tt = 0
+                        # Go through all the valid time stamp that employee has
+                        for j in range(0, len(temp_time_list), 2):
+                            temp_date_start = f"{temp_time_list[j][1]}/{temp_time_list[j][2]}/{temp_time_list[j][3]}"
+                            temp_time_start = f"{temp_time_list[j][4]}:{temp_time_list[j][5]}"
+                            temp_date_end = (f"{temp_time_list[j + 1][1]}/{temp_time_list[j + 1][2]}"
+                                             f"/{temp_time_list[j + 1][3]}")
+                            temp_time_end = f"{temp_time_list[j + 1][4]}:{temp_time_list[j + 1][5]}"
+
+                            # Cost time in min
+                            cost_min_tt += time_correct(temp_time_list[j][4], temp_time_list[j][5],
+                                                        temp_time_list[j + 1][4],temp_time_list[j + 1][5])
+
+                            # Compare to see if the start time is the earliest start time.
+                            if template[row, 11] > temp_date_start:
+                                template[row, 11] = temp_date_start
+                                template[row, 12] = temp_time_start
+                            elif template[row, 11] == temp_date_start:
+                                if template[row, 12] > temp_time_start:
+                                    template[row, 12] = temp_time_start
+
+                            # Compare to see if the end time is the latest start time.
+                            if template[row, 13] < temp_date_end:
+                                template[row, 13] = temp_date_end
+                                template[row, 14] = temp_time_end
+                            elif template[row, 13] == temp_date_end:
+                                if template[row, 14] < temp_time_end:
+                                    template[row, 14] = temp_time_end
+
+                        template[row, 16] = int(template[row, 16] * template[row, 10] + cost_min_tt) / (
+                                    template[row, 10] + 1)
+                        template[row, 10] += 1
+
+                    # Does not have the record before.
+                    else:
+                        row = np.where(template[:, 12] == "0.0")[0][0]
+                        template[row, 0] = SO_key
+                        template[row, 2] = workid_key
+                        template[row, 3] = employee_key
+                        template[row, 10] = 1
+                        cost_min_tt = 0
+                        for j in range(0, len(temp_time_list), 2):
+
+                            temp_date_start = f"{temp_time_list[j][1]}/{temp_time_list[j][2]}/{temp_time_list[j][3]}"
+                            temp_time_start = f"{temp_time_list[j][4]}:{temp_time_list[j][5]}"
+                            temp_date_end = (f"{temp_time_list[j + 1][1]}/{temp_time_list[j + 1][2]}"
+                                             f"/{temp_time_list[j + 1][3]}")
+                            temp_time_end = f"{temp_time_list[j + 1][4]}:{temp_time_list[j + 1][5]}"
+
+                            cost_min_tt += time_correct(temp_time_list[j][4], temp_time_list[j][5],
+                                                        temp_time_list[j + 1][4],
+                                                        temp_time_list[j + 1][5])
+
+                            # Compare to see if the start time is the earliest start time.
+                            if template[row, 11] > temp_date_start:
+                                template[row, 11] = temp_date_start
+                                template[row, 12] = temp_time_start
+                            elif template[row, 11] == temp_date_start:
+                                if template[row, 12] > temp_time_start:
+                                    template[row, 12] = temp_time_start
+
+                            # Compare to see if the end time is the latest start time.
+                            if template[row, 13] < temp_date_end:
+                                template[row, 13] = temp_date_end
+                                template[row, 14] = temp_time_end
+                            elif template[row, 13] == temp_date_end:
+                                if template[row, 14] < temp_time_end:
+                                    template[row, 14] = temp_time_end
+
+                        template[row, 16] = cost_min_tt
+
+    print(template)
 
 
 if __name__ == '__main__':
-    # main()
+    main()
